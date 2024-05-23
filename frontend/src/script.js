f410c81bd34c11da647e9900be64bab271ab57c5
hari <shuklahari189@gmail.com>
2024-05-23 16:07:52 +0530

    removed backend no backend hell yeag

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -8,21 +8,28 @@ const lentghOutput = document.getElementById("lentghOutput");
 length.addEventListener("change", function (e) {
     lentghOutput.innerHTML = length.value;
 });
+
 generate.addEventListener("click", async function () {
     const passLength = length.value;
     const pass = input.value;
-    const response = await fetch(
-        "https://backendonrender-4f9p.onrender.com/hashStr",
-        {
-            headers: { pass: pass, length: passLength },
-        }
-    );
-    const hashedPass = await response.text();
-    ``;
-    hashOutput.innerHTML = hashedPass;
+    const hasshedPass = await hashString(pass);
+    hashOutput.innerHTML = hasshedPass.slice(0, passLength);
     copy.className =
         "visible border rounded ml-2 px-1 text-white hover:bg-[#4b4b4b] focus:ring-2 ring-[#3d8680] translate-y-[-4px]";
 });
+
 copy.addEventListener("click", function () {
     navigator.clipboard.writeText(hashOutput.innerHTML);
 });
+
+async function hashString(str) {
+    const hashBuffer = await crypto.subtle.digest(
+        "SHA-1",
+        new TextEncoder().encode(str)
+    );
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray
+        .map((byte) => byte.toString(16).padStart(2, "0"))
+        .join("");
+    return hashHex;
+}
